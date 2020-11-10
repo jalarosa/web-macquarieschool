d70c16dde06d52bd783eae3992b220cd15a5d00e
jlarosa-utn <jlarosa@est.utn.frba.edu.ar>
2020-11-10 00:42:42 -0300

    add cache for events

--- src/db/Event/index.ts
+++ src/db/Event/index.ts
@@ -1,10 +1,28 @@
 import { DbClient } from '../index';
+import { ExpirationStrategy, MemoryStorage } from "node-ts-cache";
+const myCache = new ExpirationStrategy(new MemoryStorage());
+
+export type EventData = {
+    _id: string,
+    title: string,
+    description: string
+}
 
 class Event {
 
-    public static fetch() {
+    public static async fetch() {
+        const cacheEvents = await myCache.getItem<EventData>("events");
+        if (cacheEvents) {
+            return cacheEvents;
+        }
         try {
-            return new DbClient().getData("events", 10);
+            return new DbClient().getData("events", 10).then((events) => {
+                myCache.setItem("events", events, {  ttl: 86400 });
+                return new Promise((resolve, reject) => resolve(events));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         } catch (error) {
             console.log("Unable to connect to db");
         }
